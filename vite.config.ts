553b3fe0e063071de94c8380c931271f6cbe02fc
lizardkingLK <53265303+lizardkingLK@users.noreply.github.com>
2024-10-13 11:10:05 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   build: {
     lib: {
       entry: "./src/index.ts",
-      name: "vite-react-ts-button",
+      name: "react-count-until",
       fileName: (format) => `index.${format}.js`,
       formats: ["cjs", "es"],
     },
